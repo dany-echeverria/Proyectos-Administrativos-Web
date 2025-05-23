0ad6a19bbebf7ab31bc07eebc54173160e060446
Daniel Echeverria <alejandro.garciae@uanl.edu.mx>
2025-05-22 18:28:34 -0600

    Agreganotodoss loa rchivooooss

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
+# src/app.py
+
 from flask import Flask, render_template, session, redirect, url_for
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
+from src.config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 from db import db
 from routes.create import create_bp
 from routes.verinformacion import verinformacion_bp
 from routes.auth import auth_bp
 from routes.actualizar import actualizar_bp
-from routes.eliminar import eliminar_bp 
+from routes.eliminar import eliminar_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
@@ -16,8 +18,8 @@ app.secret_key = '1234'
 app.register_blueprint(create_bp, url_prefix='/crear')
 app.register_blueprint(verinformacion_bp, url_prefix='/ver')
 app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(actualizar_bp, url_prefix='/actualizar') 
-app.register_blueprint(eliminar_bp, url_prefix='/eliminar') 
+app.register_blueprint(actualizar_bp, url_prefix='/actualizar')
+app.register_blueprint(eliminar_bp, url_prefix='/eliminar')
 
 db.init_app(app)
 
@@ -33,6 +35,7 @@ def bienvenida():
         return render_template('proveedores.html')  # Página de bienvenida
     else:
         return redirect(url_for('auth.login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/config.py
+++ src/config.py
@@ -1,2 +1,4 @@
+# src/config.py
+
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://414551:D4ZdtmfVs_.66n2@mysql-danielecheverria.alwaysdata.net/danielecheverria_datos'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
